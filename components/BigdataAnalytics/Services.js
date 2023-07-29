d040f8128f001b904e7daf81ce8dbec958ab372a
hendOkla <75433172+hendOkla@users.noreply.github.com>
2023-07-29 13:32:31 +0300

    Update Services.js

--- components/BigdataAnalytics/Services.js
+++ components/BigdataAnalytics/Services.js
@@ -112,7 +112,7 @@ export default function Services()  {
                                 <button className="btn btn-primary" onClick={(()=>{
                                     checkout({
                                         lineItems:[{
-                                            price:"price_1NZ92nD9XEKkDfre5w75hHES",
+                                            price:"price_1NZARCD9XEKkDfre0sfJpY1J",
                                             quantity:1
                                         }]
                                     });
