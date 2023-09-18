0e171e8884dd525ca42ac5ae120ebf0fe2af29e3
Hendokla99 <hendprog@gmail.com>
2023-09-18 06:58:17 +0300

    update7 18:9

--- pages/api/init.js
+++ pages/api/init.js
@@ -1,5 +1,4 @@
 import { Client, resources } from 'coinbase-commerce-node';
-import { products } from '../../data.js';
 
 Client.init(String(process.env.COINBASE_API));
 const { Charge } = resources;
@@ -8,25 +7,18 @@ const coinInitRoute = async(req, res) => {
 
   const { id } = req.body
 
-  const product = products.find(product => product.id === id)
 
   try {
 
     const chargeData = {
-      name: product.name,
-      description: product.description,
+      name: 'Pro',
+      description: "test",
       pricing_type: "fixed_price",
       local_price: {
-        amount: product.price,
-        currency: product.currency,
-      },
-      metadata: {
-        id: product.id,
-        userID: 1
+        amount: 25,
+        currency: 'USD',
       },
 
-      redirect_url: '6figure-earner.world/pay?success=true',
-      cancel_url:'6figure-earner.world/pay?canceled=true',
     };
 
     const charge = await Charge.create(chargeData);
