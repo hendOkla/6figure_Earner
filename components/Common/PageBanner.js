e8b412d0b6e56c50ddabf95d442ebaf9070df885
Hendokla99 <hendprog@gmail.com>
2023-10-13 00:50:22 +0300

    update13:10

--- components/Common/PageBanner.js
+++ components/Common/PageBanner.js
@@ -41,5 +41,4 @@ const PageBanner = ({pageTitle}) => {
         </>
     );
 }
-
 export default PageBanner;
--- pages/showLesson.js
+++ pages/showLesson.js
@@ -7,10 +7,6 @@ import { useRouter } from 'next/router';
 import { getDictionary } from "getDictionary";
 import axios from "axios";
 
-
-
-
- 
 const BlogDetails = () => {
     const videoKey = Date.now();
     const router = useRouter();
@@ -24,9 +20,20 @@ const BlogDetails = () => {
 
 
     useEffect(()=>{
-      
-        
-    },[]);
+        const videoId  = localStorage.getItem('lesson_id');  
+        axios.get(`/api/edit-lesson/${videoId}`).then(res=>{
+            if(res.data.status === 200){
+                setLesson(res.data.lesson);
+            }
+        }); 
+
+        //for translation 
+        async function fetchTranslations() {
+            const translations = await getDictionary(locale);
+            setTranslations(translations);
+        }
+        fetchTranslations();
+    },[URL]);
 
     return (
         <>
@@ -34,8 +41,28 @@ const BlogDetails = () => {
                 <>
                     <Navbar />
                     <PageBanner pageTitle={LessonInput[`name_${locale}`]} /> 
-                   
-                   
+                    <div className="blog-details-area ptb-80">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-8 col-md-12">
+                                    <div className="blog-details-desc">
+                                        <div className="article-image">
+                                            <video key={videoKey} width={'100%'} controls controlsList="nodownload" onContextMenu={(e) => e.preventDefault()}>
+                                                <source src={`http://127.0.0.1:8000/${(locale==='ar')? LessonInput.video :LessonInput[`video_${locale}`]}`} />
+                                                Your browser does not support the video tag.
+                                            </video>
+                                        </div>
+                                        <div className="article-content">                                    
+                                            <p>{LessonInput[`description_${locale}`]}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-4 col-md-12">
+                                    <BlogSidebar2 />                           
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <Footer />
                 </>
             ) : (
@@ -44,7 +71,4 @@ const BlogDetails = () => {
         </>
     )
 }
-
-
-
 export default BlogDetails;
